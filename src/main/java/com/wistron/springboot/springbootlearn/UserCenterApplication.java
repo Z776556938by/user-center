ef2b1ca7fd962f02b60995cc3f9be6d2409fa4f6
Lucas Y Zhang <k21064736@wistron.com>
2021-10-12 11:39:28 +0800

    feat:stream的测试学习

--- src/main/java/com/wistron/springboot/springbootlearn/UserCenterApplication.java
+++ src/main/java/com/wistron/springboot/springbootlearn/UserCenterApplication.java
@@ -3,7 +3,10 @@ package com.wistron.springboot.springbootlearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import tk.mybatis.spring.annotation.MapperScan;
+
+import java.util.function.Consumer;
 
 
 @MapperScan("com.wistron")
@@ -12,4 +15,5 @@ public class UserCenterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserCenterApplication.class, args);
 	}
+
 }
